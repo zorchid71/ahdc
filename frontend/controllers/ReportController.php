882c83c85f54c9ff3b2db3b8cb0b4ae5459d4164
john <john@DESKTOP-1KNVB4P>
2015-09-08 19:57:21 +0700

    Merge origin/master
    Conflicts:
    	README.md

--- frontend/controllers/ReportController.php
+++ frontend/controllers/ReportController.php
@@ -2,11 +2,33 @@
 
 namespace frontend\controllers;
 
-class ReportController extends \yii\web\Controller
-{
-    public function actionIndex()
-    {
+use yii;
+use kartik\grid\GridView;
+
+class ReportController extends \yii\web\Controller {
+
+    public function actionIndex() {
         return $this->render('index');
+    }
+
+    public function actionReport1() {
+        $sql = "SELECT HOSPCODE as 'หน่วยบริการ',DATE_SERV as 'วันที่บริการ',ANCPLACE as 'สถานที่คลอด' from anc
+WHERE DATE_SERV >'2014-10-1' and ANCPLACE is not NULL limit 10 ";
+        //$rawData = \yii::$app->db->createCommand($sql)->queryAll();
+        //print_r($rawData);
+        try {
+            $rawData = \yii::$app->db->createCommand($sql)->queryAll();
+        } catch (\yii\db\Exception $e) {
+            throw new \yii\web\ConflictHttpException('sql error');
+        }
+        $dataProvider = new \yii\data\ArrayDataProvider([
+            'allModels' => $rawData,
+            'pagination' => FALSE,
+        ]);
+
+        return $this->render('report1', [
+                    'datProvider' => $dataProvider
+        ]);
     }
 
 }
--- sql/q1.sql
+++ sql/q1.sql
@@ -1,2 +1,2 @@
-SELECT * from anc
-WHERE DATE_SERV >'2014-10-1'
+SELECT HOSPCODE as 'หน่วยบริการ',DATE_SERV as 'วันที่บริการ',ANCPLACE as 'สถานที่คลอด' from anc
+WHERE DATE_SERV >'2014-10-1' and ANCPLACE is not NULL
